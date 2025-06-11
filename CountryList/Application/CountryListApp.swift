d3a2a754e59a82fd8199ffb44422a342c068064c
Neel Patel <neel.p@tatvasoft.com>
2025-06-11 08:47:08 +0530

    - initial commit

---
+++ CountryList/Application/CountryListApp.swift
@@ -0,0 +1,20 @@
+//
+//  CountryListApp.swift
+//  CountryList
+//
+//  Created by Neel on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CountryListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ CountryListTests/CountryListTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountryListTests.swift
+//  CountryListTests
+//
+//  Created by Neel on 10/06/25.
+//
+
+import Testing
+@testable import CountryList
+
+struct CountryListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
